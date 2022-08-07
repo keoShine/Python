199391703db56c572bc8d60176f608f4e4b6ea72
keoShine <keoShine@gmail.com>
2022-08-07 01:23:51 -0400

    Attempt to minimize number of lines

--- Collatz.py
+++ Collatz.py
@@ -1,25 +1,18 @@
-def collatz(n):
-    return int(n/2) if n % 2 == 0 else 3*n + 1
+collatz = lambda n : 3 * n + 1 if n % 2 else int(n/2)
 
-collatz_dict = {
-    1: 0
-}
+collatz_dict = {1: 0}
 
-temp = []
-counter = 0
+bound = 1000
 
-for i in range(2, 50):
+for i in range(2, bound):
+    temp = []
     j = i
-    while j not in collatz_dict.keys():
+    while j not in collatz_dict:
         temp.append(j)
         j = collatz(j)
-        counter += 1
-    for k in range(len(temp)):
-        collatz_dict.update({temp[k]: collatz_dict.get(j) + counter - k})
-    temp = []
-    counter = 0
+    tdict = dict(zip(temp, list(range(len(temp) + collatz_dict[j], collatz_dict[j] - 1, -1))))
+    collatz_dict.update(tdict)
 
-collatz_dict = dict(sorted(collatz_dict.items()))
-for i in collatz_dict:
-    print(str(i) + ": " + str(collatz_dict[i]))
+for i in range(1, bound + 1):
+    print("{:d}: {:d}".format(i, collatz_dict[i]))
 
